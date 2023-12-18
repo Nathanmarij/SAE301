1f2be9e6f237828187d41cbd063e50a954cbf0f4
Gabzeeer <laveniergabriel@gmail.com>
2023-12-18 11:58:18 +0100

    page admin css

--- sae301/administration.php
+++ sae301/administration.php
@@ -1,52 +1,68 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/style.css" type="text/css" rel="stylesheet" />
+    <link href="css/style2.css" type="text/css" rel="stylesheet" />
     <script src="js/script.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
+    <link rel="icon" href="favicon.ico">
     <title>Unicert 2024 - Administration</title>
     <?php
-    include ("config.php");
+    include("config.php");
     session_start();
     try {
-        $bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nombase , $utilisateur , $mdp);
+        $bdd = new PDO('mysql:host=' . $hote . ';port=' . $port . ';dbname=' . $nombase, $utilisateur, $mdp);
     } catch (Exception $e) { // sinon
         die("erreur"); // ecrire 'erreur'
     }
 
     if (isset($_SESSION["user"])) { // si l'utilisateur est connecté
-        $iduser=$_SESSION["user"];
+        $iduser = $_SESSION["user"];
         ?>
         <h1>Vous êtes connecté</h1>
-        <a href="deconnexion.php"><h1>deconnexion</h1></a>
-        <?php 
-    $requete = 'SELECT id_user, nom, prenom FROM user WHERE id_user = :id';
-    $statement = $bdd->prepare($requete);
-    $statement->bindParam(':id', $iduser, PDO::PARAM_INT);
-    $statement->execute();
-    $identite = $statement->fetchAll(PDO::FETCH_ASSOC);
-        } else { // sinon
+        <a href="deconnexion.php">
+            <h1>deconnexion</h1>
+        </a>
+        <?php
+        $requete = 'SELECT id_user, nom, prenom FROM user WHERE id_user = :id';
+        $statement = $bdd->prepare($requete);
+        $statement->bindParam(':id', $iduser, PDO::PARAM_INT);
+        $statement->execute();
+        $identite = $statement->fetchAll(PDO::FETCH_ASSOC);
+    } else { // sinon
     }
     ?>
 </head>
+
 <body>
-<header>
+    <header>
         <div class="header">
             <div class="logo">
-                <img src="image/logo.png" alt="UNICEF logo">
+                <img src="image/logouni.png" alt="UNICEF logo">
             </div>
             <div class="btn-nav">
                 <div class="navbar">
-                    <a href="#actions"><h2>Billeterie</h2></a>
-                    <a href="forum.php"><h2>Forum</h2></a>
-                    <a href="#interventions"><h2>Contact</h2></a>
+                    <a href="#actions">
+                        <h2>Billeterie</h2>
+                    </a>
+                    <a href="forum.php">
+                        <h2>Forum</h2>
+                    </a>
+                    <a href="#interventions">
+                        <h2>Contact</h2>
+                    </a>
                 </div>
             </div>
             <div class="btn-nav">
                 <div class="compte">
-                    <a href="inscription.php"><h2>Inscription</h2></a>
-                    <a href="connexion.php"><h2>Connexion</h2></a>
+                    <a href="inscription.php">
+                        <h2>Inscription</h2>
+                    </a>
+                    <a href="connexion.php">
+                        <h2>Connexion</h2>
+                    </a>
                 </div>
             </div>
         </div>
@@ -56,41 +72,41 @@
             <h3>Ajouter un festival</h3>
             <form action="festival.php" method="POST">
                 <li>
-                <label for="nom">Nom<label>
-                <input type="text" id="nom" name="nom">
+                    <label for="nom">Nom<label>
+                            <input type="text" id="nom" name="nom">
                 </li>
                 <li>
-                <label for="date">Date<label>
-                <input type="datetime" id="date" name="date">
+                    <label for="date">Date<label>
+                            <input type="datetime" id="date" name="date">
                 </li>
-                
+
                 <li>
-                <label for="nbplace">Nombre de places disponibles<label>
-                <input type="number" id="nbplace" name="nbplace">
-                </li>
-                <li>
-                <label for="lieu">Lieu<label>
-                <input type="text" id="lieu" name="lieu">
+                    <label for="nbplace">Nombre de places disponibles<label>
+                            <input type="number" id="nbplace" name="nbplace">
                 </li>
                 <li>
-                <label for="artiste">Artiste 1<label>
-                <input type="text" id="artiste" name="artiste">
+                    <label for="lieu">Lieu<label>
+                            <input type="text" id="lieu" name="lieu">
                 </li>
                 <li>
-                <label for="artiste">Artiste 2<label>
-                <input type="text" id="artiste" name="artiste">
+                    <label for="artiste">Artiste 1<label>
+                            <input type="text" id="artiste" name="artiste">
                 </li>
                 <li>
-                <label for="artiste">Artiste 3<label>
-                <input type="text" id="artiste" name="artiste">
+                    <label for="artiste">Artiste 2<label>
+                            <input type="text" id="artiste" name="artiste">
                 </li>
                 <li>
-                <label for="artiste">Artiste 4<label>
-                <input type="text" id="artiste" name="artiste">
+                    <label for="artiste">Artiste 3<label>
+                            <input type="text" id="artiste" name="artiste">
                 </li>
                 <li>
-                <label for="artiste">Artiste 5<label>
-                <input type="text" id="artiste" name="artiste">
+                    <label for="artiste">Artiste 4<label>
+                            <input type="text" id="artiste" name="artiste">
+                </li>
+                <li>
+                    <label for="artiste">Artiste 5<label>
+                            <input type="text" id="artiste" name="artiste">
                 </li>
                 <button type="submit">Valider</button>
                 <button type="reset">Réinitialiser</button>
@@ -98,7 +114,8 @@
         </article>
     </section>
     <footer>
-        
+
     </footer>
 </body>
+
 </html>
