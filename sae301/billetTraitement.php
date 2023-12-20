fa2f9791b99276b3ab1a3db706073d22b5345969
Nathanmarij <marijnissennathan@gmail.com>
2023-12-20 16:42:42 +0100

    Changement  billeterie

--- sae301/billetTraitement.php
+++ sae301/billetTraitement.php
@@ -1,17 +1,19 @@
 <?php 
 include("config.php");
+session_start();
     try {
         $bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nombase , $utilisateur , $mdp);
     } catch (Exception $e) { // sinon
         die("erreur");
         // ecrire 'erreur'
     }
-    print_r($_POST);
+    print_r($_POST["nb_places"]);
+    for ($i = 1; $i <= $_POST["nb_places"]; $i++) {
+        $iduser = $_SESSION['user'];
 $type = $_POST["type"];
-$nb_places = $_POST["nb_places"];
-$id_user = $_POST["id_user"];
-$requete = $bdd->prepare("INSERT INTO billetterie (type_place, nb_places, id_user) VALUES(?,?,?)"); // insérer dans la table commentaire les valeurs envoyés par le formulaire
-$requete->execute([$type, $nb_places, $id_user]); // executer la requete
+$requete = $bdd->prepare("INSERT INTO billetterie (type_place, id_user) VALUES(?,?)"); // insérer dans la table commentaire les valeurs envoyés par le formulaire
+$requete->execute([$type, $iduser]); // executer la requete
 $billet = $requete->fetch();
-header("Location:index.php");
+    }
+    header("Location:confirmation.html");
 ?>
