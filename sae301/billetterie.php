d4fd5f67e0c52bd240fae354bad9503b677ab7df
Nathanmarij <marijnissennathan@gmail.com>
2023-12-21 10:34:31 +0100

    Update billetterie.php

--- sae301/billetterie.php
+++ sae301/billetterie.php
@@ -10,73 +10,53 @@
     <script src="js/script2.js"></script>
     <title>Unicert 2024 - Billeterie</title>
     <?php
-    include("config.php");
-    session_start();
-    try {
-        $bdd = new PDO('mysql:host=' . $hote . ';port=' . $port . ';dbname=' . $nombase, $utilisateur, $mdp);
-    } catch (Exception $e) { // sinon
-        die("erreur"); // ecrire 'erreur'
-    }
+include ("config.php");
+session_start();
+try {
+$bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nombase , $utilisateur , $mdp);
+} catch (Exception $e) { // sinon
+die("erreur"); // ecrire 'erreur'
+}
 
-    if (isset($_SESSION["user"])) { // si l'utilisateur est connecté
-        $iduser = $_SESSION["user"];
-        ?>
-        <h1>Vous êtes connecté</h1>
-        <a href="deconnexion.php">
-            <h1>deconnexion</h1>
-        </a>
-        <?php
-        $requete = 'SELECT id_user, nom, prenom FROM user WHERE id_user = :id';
-        $statement = $bdd->prepare($requete);
-        $statement->bindParam(':id', $iduser, PDO::PARAM_INT);
-        $statement->execute();
-        $identite = $statement->fetchAll(PDO::FETCH_ASSOC);
-    } else { // sinon
-    }
-    ?>
+if (isset($_SESSION["user"])) { // si l'utilisateur est connecté
+$iduser=$_SESSION["user"]; 
+$requete = 'SELECT id_user, nom, prenom FROM user WHERE id_user = :id';
+$statement = $bdd->prepare($requete);
+$statement->bindParam(':id', $iduser, PDO::PARAM_INT);
+$statement->execute();
+$identite = $statement->fetchAll(PDO::FETCH_ASSOC);
+} else { // sinon
+}
+?>
 </head>
 
 <body>
-    <header>
+<header>
         <div class="header">
             <div class="btn-nav navbar">
                 <!-- Boutons de navigation -->
-                <a href="#actions">
-                    <h2>Billetterie</h2>
-                </a>
-                <a href="forum.php">
-                    <h2>Forum</h2>
-                </a>
-                <a href="#interventions">
-                    <h2>Contact</h2>
-                </a>
+                <a href="#actions"><h2>Billetterie</h2></a>
+                <a href="forum.php"><h2>Forum</h2></a>
+                <a href="#interventions"><h2>Contact</h2></a>
             </div>
             <div class="logo">
                 <!-- Logo -->
-                <a href="index.php">
-                    <img src="image/logo.png" alt="UNICEF logo">
-                </a>
+                <img src="image/logo.png" alt="UNICEF logo">
             </div>
             <div class="btn-nav compte">
-                <!-- Boutons d'inscription et de connexion -->
-                <?php
-                if (isset($_SESSION["user"])) { // si l'utilisateur est connecté
+                <!-- Boutons d'inscription et de connexion --><?php
+                 if (isset($_SESSION["user"])) { // si l'utilisateur est connecté
                     $requete = 'SELECT id_user, nom, prenom FROM user WHERE id_user = :id';
                     $statement = $bdd->prepare($requete);
                     $statement->bindParam(':id', $iduser, PDO::PARAM_INT);
                     $statement->execute();
                     $identite = $statement->fetchAll(PDO::FETCH_ASSOC);
-                    $iduser = $_SESSION["user"];
-                    echo '<a href="deconnexion.php"><h2>Déconnexion</h2></a>';
-                } else { ?>
-                    <a href="inscription.php">
-                        <h2>Inscription</h2>
-                    </a>
-                    <a href="connexion.php">
-                        <h2>Connexion</h2>
-                    </a>
-                    <?php
-                } ?>
+                $iduser=$_SESSION["user"];
+                echo '<a href="deconnexion.php"><h2>Déconnexion</h2></a>';}
+                else {?>
+                <a href="inscription.php"><h2>Inscription</h2></a>
+                <a href="connexion.php"><h2>Connexion</h2></a><?php
+                }?>
 
 
             </div>
@@ -88,42 +68,30 @@
             <h5>Prix du billet</h5>
             <p>Explication</p>
             <label for="quantite-billet1">Quantité:</label>
-            <select id="quantite-billet1" name="quantite-billet1">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <a href="confirmation.html" class="lien">Acheter<a>
+            <form method="POST" class="form" <?php echo isset($_SESSION["user"]) ? 'action="billetTraitement.php"' : 'action="inscription.php"'; ?>>
+            <input type="number" name="nb_places" id="nb_places" value="0" min="0" max="100"/>
+            <input type="HIDDEN" name="type" value="Apres-midi">
+            <input type="submit" id="submit" name="submit" value="Réserver" class="lien"></form>
         </article>
         <article class="billet">
             <h3>Billet 2</h3>
             <h5>Prix du billet</h5>
             <p>Explication</p>
             <label for="quantite-billet1">Quantité:</label>
-            <select id="quantite-billet1" name="quantite-billet1">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <a href="confirmation.html" class="lien">Acheter<a>
+            <form method="POST" class="form" <?php echo isset($_SESSION["user"]) ? 'action="billetTraitement.php"' : 'action="inscription.php"'; ?>>
+            <input type="number" name="nb_places" id="nb_places" value="0" min="0" max="100"/>
+            <input type="HIDDEN" name="type" value="Soiree">
+            <input type="submit" id="submit" name="submit" value="Réserver" class="lien"></form>
         </article>
         <article class="billet">
             <h3>Billet 3</h3>
             <h5>Prix du billet</h5>
             <p>Explication</p>
             <label for="quantite-billet1">Quantité:</label>
-            <select id="quantite-billet1" name="quantite-billet1">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-            <a href="confirmation.html" class="lien">Acheter<a>
+            <form method="POST" class="form" <?php echo isset($_SESSION["user"]) ? 'action="billetTraitement.php"' : 'action="inscription.php"'; ?>>
+            <input type="number" name="nb_places" id="nb_places" value="0" min="0" max="100"/>
+            <input type="HIDDEN" name="type" value="Journee">
+            <input type="submit" id="submit" name="submit" value="Réserver" class="lien"></form>
         </article>
     </section>
     <footer>
