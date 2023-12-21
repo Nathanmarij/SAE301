39333e20aa4d1b527ea73e1a8e69052cb070815a
Juzanivan <juleszanivan16@gmail.com>
2023-12-21 16:52:57 +0100

    Profil + Statut Admin Terminés

--- sae301/forum.php
+++ sae301/forum.php
@@ -1,3 +1,7 @@
+<?php
+include("config.php");
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -10,154 +14,136 @@
     <title>forum</title>
 </head>
 <body>
-<header>
+    <header>
         <div class="header">
-            <div class="logo">
-            <a href="index.php">
-            <!-- Logo -->
-            <img src="image/logo.png" alt="UNICEF logo">
-            </a>
-            </div>
-            <div class="nav-container">
-            <div class="btn-nav">
+            <div class="btn-nav navbar">
                 <!-- Boutons de navigation -->
-                <a href="billetterie.php">
-                    <h2>Billetterie</h2>
-                </a>
-                <a href="forum.php">
-                    <h2>Forum</h2>
-                </a>
-                <a href="Infos Pratiques.php">
-                    <h2>Infos Pratiques</h2>
-                </a>
+                <a href="billeterie.php"><h2>Billetterie</h2></a>
+                <a href="forum.php"><h2>Forum</h2></a>
+                <a href="#interventions"><h2>Contact</h2></a>
             </div>
+            <div class="logo">
+                <!-- Logo -->
+                <img src="image/logo.png" alt="UNICEF logo">
             </div>
-
-            <div class="btn-compte">
-                <!-- Boutons d'inscription et de connexion -->
-                <?php
-                if (isset($_SESSION["user"])) { // si l'utilisateur est connecté
-                    $requete = 'SELECT id_user, nom, prenom FROM user WHERE id_user = :id';
-                    $statement = $bdd->prepare($requete);
-                    $statement->bindParam(':id', $iduser, PDO::PARAM_INT);
-                    $statement->execute();
-                    $identite = $statement->fetchAll(PDO::FETCH_ASSOC);
-                    $iduser = $_SESSION["user"];
-                    echo '<a href="deconnexion.php"><h2>Déconnexion</h2></a>';
-                } else { ?>
-                    <a href="inscription.php">
-                        <h2>Inscription</h2>
-                    </a>
-                    <a href="connexion.php">
-                        <h2>Connexion</h2>
-                    </a>
-                    <?php
-                } ?>
+            <div class="btn-nav compte">
+            <?php
+            if (isset($_SESSION["user"])) { // si l'utilisateur est connecté
+                echo '<a href="profil.php"><h2>Profil</h2></a>';
+            } else { ?>
+                <a href="inscription.php"><h2>Inscription</h2></a>
+                <a href="connexion.php"><h2>Connexion</h2></a><?php
+            }?>
             </div>
         </div>
     </header>
-
     <div class="container">
     <?php
-include("config.php");
-session_start();
-date_default_timezone_set('Europe/Paris');
-setlocale(LC_TIME, 'fr_FR.UTF-8', 'French_France.1252');
+    date_default_timezone_set('Europe/Paris');
+    setlocale(LC_TIME, 'fr_FR.UTF-8', 'French_France.1252');
 
-try {
-    $bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nombase, $utilisateur, $mdp);
-} catch (Exception $e) {
-    die("erreur");
-}
-
-function getForumComments($bdd) {
-    $requete = 'SELECT id_ask, id_user, content, date_posted FROM forum ORDER BY id_ask DESC';
-    $statement = $bdd->prepare($requete);
-    $statement->execute();
-    return $statement->fetchAll(PDO::FETCH_ASSOC);
-}
-
-$forum = getForumComments($bdd);
-
-if (isset($_SESSION["user"])) { ?>
-
-    <form action="forTraitement.php" method="POST" class="commentaire">
-        <input type="text" placeholder="Posez une question" id="content" name="content" class="ecrir" required>
-        <input type="submit" id="submit" name="submit" value="Commenter">
-    </form>
-
-<?php }
-
-$months = [
-    "January" => "Janvier", "February" => "Février", "March" => "Mars",
-    "April" => "Avril", "May" => "Mai", "June" => "Juin",
-    "July" => "Juillet", "August" => "Août", "September" => "Septembre",
-    "October" => "Octobre", "November" => "Novembre", "December" => "Décembre"
-];
-
-foreach ($forum as $val) {
-    $requete = 'SELECT nom, prenom FROM user WHERE id_user = :id_user';
-    $statement = $bdd->prepare($requete);
-    $statement->bindValue(':id_user', $val["id_user"], PDO::PARAM_INT);
-    $statement->execute();
-    $commentateur = $statement->fetch(PDO::FETCH_ASSOC);
-
-    echo '<fieldset class="comment-fieldset"><legend><b>';
-    echo htmlspecialchars($commentateur["prenom"]) . " " . htmlspecialchars($commentateur["nom"]);
-    echo "</b></legend>";
-    echo htmlspecialchars($val["content"]);
-    echo "<br>";
-
-    $date = new DateTime($val["date_posted"]);
-    $formattedDate = $date->format('d F Y à H\hi');
-    $month = $date->format('F');
-    $formattedDate = str_replace($month, $months[$month], $formattedDate);
-
-    echo "<i>Posté le " . $formattedDate . "</i><br>";
-
-    if (isset($_SESSION["user"]) && $_SESSION["user"] == $val["id_user"]) {
-        echo '<button class="delete-btn" onclick="deleteComment(' . $val['id_ask'] . ')"><img src="image/poubelle.png" alt="Supprimer"></button>';
+    try {
+        $bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nombase, $utilisateur, $mdp);
+    } catch (Exception $e) {
+        die("erreur");
     }
 
-    if (isset($_SESSION["user"])) {
-        echo '<button onclick="toggleReplyForm(' . $val['id_ask'] . ')" id="reply-btn-' . $val['id_ask'] . '">Répondre</button>';
-        echo '<form id="reply-form-' . $val['id_ask'] . '" action="reponseTraitement.php" method="POST" style="display:none;">';
-        echo '<input type="hidden" name="id_ask" value="' . $val['id_ask'] . '">';
-        echo '<input type="text" name="contenu" placeholder="Votre réponse">';
-        echo '<input type="submit" value="Envoyer">';
-        echo '</form>';
+    function getForumComments($bdd) {
+        $requete = 'SELECT id_ask, id_user, content, date_posted FROM forum ORDER BY id_ask DESC';
+        $statement = $bdd->prepare($requete);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
-    
-    $requete = 'SELECT r.id_reponse, r.contenu, r.date_posted, r.id_user, u.nom, u.prenom FROM reponse r JOIN user u ON r.id_user = u.id_user WHERE r.id_ask = ' . $val['id_ask'];
-    $reponses = $bdd->query($requete)->fetchAll(PDO::FETCH_ASSOC);
 
-    if (count($reponses) > 0) {
-        echo '<button class="toggle-reply" onclick="toggleReplies(' . $val['id_ask'] . ')">Voir Réponses</button>';
-        echo '<div class="replies-container" id="replies-' . $val['id_ask'] . '" style="display: none;">';
+    $forum = getForumComments($bdd);
 
-        foreach ($reponses as $reponse) {
-            echo '<fieldset style="margin-left: 20px;">';
-            echo '<legend>Réponse de <b>' . htmlspecialchars($reponse['prenom']) . ' ' . htmlspecialchars($reponse['nom']) . '</b></legend>';
-            echo htmlspecialchars($reponse['contenu']);
+    if (isset($_SESSION["user"])) { ?>
 
-            $dateReponse = new DateTime($reponse["date_posted"]);
-            $formattedDateReponse = $dateReponse->format('d F Y à H\hi');
-            $monthReponse = $dateReponse->format('F');
-            $formattedDateReponse = str_replace($monthReponse, $months[$monthReponse], $formattedDateReponse);
+        <form action="forTraitement.php" method="POST" class="commentaire">
+            <input type="text" placeholder="Posez une question" id="content" name="content" class="ecrir" required>
+            <input type="submit" id="submit" name="submit" value="Commenter">
+        </form>
 
-            echo "<i><br>Posté le " . $formattedDateReponse . "</i><br>";
+    <?php }
 
-            if (isset($_SESSION["user"]) && $_SESSION["user"] == $reponse["id_user"]) {
+    $months = [
+        "January" => "Janvier", 
+        "February" => "Février", 
+        "March" => "Mars",
+        "April" => "Avril", 
+        "May" => "Mai", 
+        "June" => "Juin",
+        "July" => "Juillet", 
+        "August" => "Août", 
+        "September" => "Septembre",
+        "October" => "Octobre", 
+        "November" => "Novembre", 
+        "December" => "Décembre"
+    ];
+
+    foreach ($forum as $val) {
+        $requete = 'SELECT nom, prenom FROM user WHERE id_user = :id_user';
+        $statement = $bdd->prepare($requete);
+        $statement->bindValue(':id_user', $val["id_user"], PDO::PARAM_INT);
+        $statement->execute();
+        $commentateur = $statement->fetch(PDO::FETCH_ASSOC);
+
+        echo '<fieldset class="comment-fieldset"><legend><b>';
+        echo htmlspecialchars($commentateur["prenom"]) . " " . htmlspecialchars($commentateur["nom"]);
+        echo "</b></legend>";
+        echo htmlspecialchars($val["content"]);
+        echo "<br>";
+
+        $date = new DateTime($val["date_posted"]);
+        $formattedDate = $date->format('d F Y à H\hi');
+        $month = $date->format('F');
+        $formattedDate = str_replace($month, $months[$month], $formattedDate);
+
+        echo "<i>Posté le " . $formattedDate . "</i><br>";
+
+        if (isset($_SESSION["isAdmin"]) && $_SESSION["isAdmin"] == 1 || (isset($_SESSION["user"]) && $_SESSION["user"] == $val["id_user"])) {
+            echo '<button class="delete-btn" onclick="deleteComment(' . $val['id_ask'] . ')"><img src="image/poubelle.png" alt="Supprimer"></button>';
+        }
+
+        if (isset($_SESSION["user"])) {
+            echo '<button onclick="toggleReplyForm(' . $val['id_ask'] . ')" id="reply-btn-' . $val['id_ask'] . '">Répondre</button>';
+            echo '<form id="reply-form-' . $val['id_ask'] . '" action="reponseTraitement.php" method="POST" style="display:none;">';
+            echo '<input type="hidden" name="id_ask" value="' . $val['id_ask'] . '">';
+            echo '<input type="text" name="contenu" placeholder="Votre réponse">';
+            echo '<input type="submit" value="Envoyer">';
+            echo '</form>';
+        }
+        
+        $requete = 'SELECT r.id_reponse, r.contenu, r.date_posted, r.id_user, u.nom, u.prenom FROM reponse r JOIN user u ON r.id_user = u.id_user WHERE r.id_ask = ' . $val['id_ask'];
+        $reponses = $bdd->query($requete)->fetchAll(PDO::FETCH_ASSOC);
+
+        if (count($reponses) > 0) {
+            echo '<button class="toggle-reply" onclick="toggleReplies(' . $val['id_ask'] . ')">Voir Réponses</button>';
+            echo '<div class="replies-container" id="replies-' . $val['id_ask'] . '" style="display: none;">';
+
+            foreach ($reponses as $reponse) {
+                echo '<fieldset style="margin-left: 20px;">';
+                echo '<legend>Réponse de <b>' . htmlspecialchars($reponse['prenom']) . ' ' . htmlspecialchars($reponse['nom']) . '</b></legend>';
+                echo htmlspecialchars($reponse['contenu']);
+
+                $dateReponse = new DateTime($reponse["date_posted"]);
+                $formattedDateReponse = $dateReponse->format('d F Y à H\hi');
+                $monthReponse = $dateReponse->format('F');
+                $formattedDateReponse = str_replace($monthReponse, $months[$monthReponse], $formattedDateReponse);
+
+                echo "<i><br>Posté le " . $formattedDateReponse . "</i><br>";
+
+            if (isset($_SESSION["isAdmin"]) && $_SESSION["isAdmin"] == 1 || (isset($_SESSION["user"]) && $_SESSION["user"] == $reponse["id_user"])) {
                 echo '<button class="delete-btn" onclick="deleteResponse(' . $reponse['id_reponse'] . ')"><img src="image/poubelle.png" alt="Supprimer"></button>';
             }
-            echo '</fieldset>';
+                echo '</fieldset>';
+            }
+            echo '</div>'; // Fin du conteneur des réponses
         }
-        echo '</div>'; // Fin du conteneur des réponses
-    }
 
-    echo '</fieldset>';
-}
-?>
-</div>
+        echo '</fieldset>';
+    }
+    ?>
+    </div>
 </body>
 </html>
---
+++ sae301/profil.php
@@ -0,0 +1,168 @@
+<?php
+include("config.php");
+session_start();
+
+try {
+    $bdd = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nombase , $utilisateur , $mdp);
+} catch (Exception $e) {
+    die("erreur");
+}
+
+if (!isset($_SESSION["user"])) {
+    header("Location: connexion.php");
+    exit();
+}
+
+$iduser = $_SESSION["user"];
+$message = "";
+
+try {
+    $requete = $bdd->prepare("SELECT nom, prenom, mail, isAdmin FROM user WHERE id_user = :id_user");
+    $requete->bindParam(':id_user', $iduser, PDO::PARAM_INT);
+    $requete->execute();
+    $user = $requete->fetch(PDO::FETCH_ASSOC);
+} catch (Exception $e) {
+    die("Erreur : " . $e->getMessage());
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['verifier_mdp'])) {
+    $ancien_mdp = $_POST["ancien_mdp"];
+    $requete = $bdd->prepare("SELECT mdp FROM user WHERE id_user = :id_user");
+    $requete->bindParam(':id_user', $iduser, PDO::PARAM_INT);
+    $requete->execute();
+    $resultat = $requete->fetch(PDO::FETCH_ASSOC);
+
+    if (password_verify($ancien_mdp, $resultat['mdp'])) {
+        $message = "";
+        echo "<script>document.addEventListener('DOMContentLoaded', function() { document.getElementById('change-password-section').style.display = 'block'; });</script>";
+    } else {
+        $message = "L'ancien mot de passe est incorrect.";
+    }
+} 
+
+elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Mise à jour des autres informations
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $mail = $_POST["mail"];
+    $nouveau_mdp = password_hash($_POST["nouveau_mdp"], PASSWORD_DEFAULT);
+
+    if (!empty($_POST['nom'])) {
+        $requete = $bdd->prepare("UPDATE user SET nom = :nom WHERE id_user = :id_user");
+        $requete->bindParam(':nom', $nom);
+        $requete->bindParam(':id_user', $iduser, PDO::PARAM_INT);
+        $requete->execute();
+        echo "Votre nom a bien été mis à jour.";
+    }
+    
+    if (!empty($_POST['prenom'])) {
+        $requete = $bdd->prepare("UPDATE user SET prenom = :prenom WHERE id_user = :id_user");
+        $requete->bindParam(':prenom', $prenom);
+        $requete->bindParam(':id_user', $iduser, PDO::PARAM_INT);
+        $requete->execute();
+        echo "Votre prénom a bien été mis à jour.";
+    }
+    
+    if (!empty($_POST['mail'])) {
+        $requete = $bdd->prepare("UPDATE user SET mail = :mail WHERE id_user = :id_user");
+        $requete->bindParam(':mail', $mail);
+        $requete->bindParam(':id_user', $iduser, PDO::PARAM_INT);
+        $requete->execute();
+        echo "Votre adresse email a bien été mise à jour.";
+    }
+    
+    if (!empty($_POST['nouveau_mdp']) && !empty($_POST['confirmer_mdp']) && $_POST['nouveau_mdp'] === $_POST['confirmer_mdp']) {
+        $requete = $bdd->prepare("UPDATE user SET mdp = :mdp WHERE id_user = :id_user");
+        $requete->bindParam(':mdp', $nouveau_mdp);
+        $requete->bindParam(':id_user', $iduser, PDO::PARAM_INT);
+        $requete->execute();
+        echo "Votre mot de passe a bien été mis à jour.";
+    }
+    
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/header.css" type="text/css" rel="stylesheet" />
+    <link href="css/profil.css" type="text/css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
+    <script src="js/profil.js"></script>
+    <title>Profil</title>
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="btn-nav navbar">
+                <!-- Boutons de navigation -->
+                <a href="billeterie.php"><h2>Billetterie</h2></a>
+                <a href="forum.php"><h2>Forum</h2></a>
+                <a href="#interventions"><h2>Contact</h2></a>
+            </div>
+            <div class="logo">
+                <!-- Logo -->
+                <img src="image/logo.png" alt="UNICEF logo">
+            </div>
+            <div class="btn-nav compte">
+            <?php
+            if (isset($_SESSION["user"])) { // si l'utilisateur est connecté
+                echo '<a href="index.php"><h2>Menu Principal</h2></a>';
+            } ?>
+            </div>
+        </div>
+    </header>
+    <h1>Modifier votre profil</h1>
+    <?php if ($message != "") echo "<p>$message</p>"; ?>
+    <form method="post" action="profil.php">
+        <div class="form-group">
+            Nom: (<b><span style="color: #0b7cff;"><?php echo $user['nom']; ?></span></b>)
+            <button type="button" onclick="toggleInput('nom')">Modifier</button>
+            <input type="text" id="nom" name="nom" placeholder="Entrez votre nouveau Nom" style="display: none;"><br>
+        </div>
+
+        <div class="form-group">
+            Prénom: (<b><span style="color: #0b7cff;"><?php echo $user['prenom']; ?></span></b>)
+            <button type="button" onclick="toggleInput('prenom')">Modifier</button>
+            <input type="text" id="prenom" name="prenom" placeholder="Entrez votre nouveau Prénom" style="display: none;"><br>
+        </div>
+
+        <div class="form-group">
+            Email: (<b><span style="color: #0b7cff;"><?php echo $user['mail']; ?></span></b>)
+            <button type="button" onclick="toggleInput('mail')">Modifier</button>
+            <input type="email" id="mail" name="mail" placeholder="Entrez votre nouveau Mail" style="display: none;"><br>
+        </div>
+
+        <div class="form-group" id="password-verified">
+            Changer de mot de passe: 
+            <button type="button" onclick="toggleInput('mdpact')">Modifier</button>
+            <div id="mdpact" style="display: none;">
+                <input type="password" id="ancien_mdp" name="ancien_mdp" placeholder="Entrez votre ancien Mot de Passe">
+                <button type="submit" name="verifier_mdp">Vérifier</button>
+            </div>
+        </div>
+
+        <div class="form-group" id="change-password-section" style="display: none;">
+            Nouveau mot de passe: 
+            <input type="password" id="nouveau_mdp" name="nouveau_mdp" placeholder="Entrez votre nouveau Mot de Passe">
+            Confirmer le nouveau mot de passe: 
+            <input type="password" id="confirmer_mdp" name="confirmer_mdp" placeholder="Répétez votre nouveau Mot de passe"><br>
+        </div>
+
+        <input id="maj" type="submit" value="Mettre à jour">
+    </form>
+    <div class="decsupp">
+        <a href="deconnexion.php" class="btn-deconnexion">Déconnexion</a>
+        <form method="post" action="suppCompte.php">
+            <input type="hidden" name="id_user" value="<?php echo $iduser; ?>">
+            <button type="submit" class="btn-supprimer" onclick="return confirm('Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.');">Supprimer mon compte</button>
+        </form>
+    </div><br>
+    <?php
+    if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == 1) {
+        ?><a href="promouvoirAdmin.php" class="btn-deconnexion">Droits Admin</a><?php
+    }
+    ?>
+</body>
+</html>
